0c16025545c232dca327d9ef964e4f3e24a3da98
maruf520 <md.maruf5201@gmail.com>
2021-09-03 11:40:43 +0600

    Cart Model created

---
+++ src/E-Shop.Models/Carts/CartItem.cs
@@ -0,0 +1,19 @@
+﻿using E_Shop.Models.Products;
+using E_Shop.Models.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Shop.Models.Carts
+{
+    public class CartItem : BaseObject
+    {
+        public long CartId { get; set; }
+        public Cart Cart { get; set; }
+        public long ProductId { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ src/E-Shop.Models/Carts/CartStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Shop.Models.Carts
+{
+    public enum CartStatus
+    {
+        Active = 0,
+        InActive = 1
+    }
+}
